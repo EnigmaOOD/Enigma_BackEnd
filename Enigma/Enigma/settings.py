90f537c55f45d7e04b478767229800ae0c905003
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2023-05-14 01:17:44 +0330

    Revert "log"
    This reverts commit 4a2a8a4553557840c31433eca4b636c65e1f40e3.

--- Enigma/Enigma/settings.py
+++ Enigma/Enigma/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,35 +155,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'MyUser.MyUser'
-
-
-LOGGING ={
-    'version':1,
-    'loggers':{
-        'django':{
-            'handlers':['file','file2'],
-            'level':'DEBUG'
-        }
-    },
-    'handlers':{
-        'file':{
-            'level':'INFO',
-            'class': 'logging.FileHandler',
-            'filename':'./logs/info.log',
-            'formatter':'simpleRe',
-        },
-        'file2':{
-            'level':'ERROR',
-            'class': 'logging.FileHandler',
-            'filename':'./logs/error.log',
-            'formatter':'simpleRe',
-        }
-    },
-    'formatters':{
-        'simpleRe': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{',
-        }
-
-    }
-}
