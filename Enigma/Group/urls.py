6073b6178a3b8bfe9f2f0527adf631936c44e424
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2023-05-16 17:25:08 +0330

    change in gorup urls

--- Enigma/Group/urls.py
+++ Enigma/Group/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from Group import views
 from django.urls import path, re_path
-from .views import CreateGroup, DeleteGroup, AddUserGroup, AmountofDebtandCredit
+from Group.views import GroupInfo, CreateGroup, DeleteGroup, AddUserGroup, AmountofDebtandCredit, ShowMembers, ShowGroups
 #just simple comment
 app_name = 'Group'
 urlpatterns = [
 
-    path('GroupInfo/', views.GroupInfo.as_view(), name='GroupInfo'),
-    path('DeleteGroup/', views.DeleteGroup.as_view(), name='DeleteGroup'),
-    path('ShowMembers/', views.ShowMembers.as_view(), name='ShowMembers'),
-    path('ShowGroups/', views.ShowGroups.as_view(), name='ShowGroups'),
+    path('GroupInfo/', GroupInfo.as_view(), name='GroupInfo'),
+    path('DeleteGroup/', DeleteGroup.as_view(), name='DeleteGroup'),
+    path('ShowMembers/', ShowMembers.as_view(), name='ShowMembers'),
+    path('ShowGroups/', ShowGroups.as_view(), name='ShowGroups'),
     path('CreateGroup/', CreateGroup.as_view(), name='CreateGroup'),
     path('AddUserGroup/', AddUserGroup.as_view(), name='AddUserGroup'),
     path('AmountofDebtandCredit/', AmountofDebtandCredit.as_view(), name='AmountofDebtandCredit'),
